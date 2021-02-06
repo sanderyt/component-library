3362cc77b9b288ff00014aaccfdd27d98da7943f
Sander van Rijsoort <hallo@svanrijsoort.nl>
2021-02-06 12:17:56 +0000

    created one command for creating new components

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+import "./Button.scss";
+
+interface Props {}
+
+export const Button: FC<Props> = () => <div></div>;
---
+++ templates/component/component.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+import "./TemplateName.scss";
+
+interface Props {}
+
+export const TemplateName: FC<Props> = () => <div></div>;
